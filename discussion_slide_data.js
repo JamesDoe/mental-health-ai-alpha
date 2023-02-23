90afe0c7c317462230fa15a839f1df37fc68e796
JamesDoe <jamesdoejr@gmail.com>
2023-02-22 23:14:06 -0500

    Add files via upload

---
+++ discussion_slide_data.js
@@ -0,0 +1,673 @@
+var data = [
+  {
+    "phrase": "Thank you for your help",
+    "comps": 1,
+    "extrs": 1
+  },
+  {
+    "phrase": "I really appreciate your kindness",
+    "comps": 1,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I'm sorry for what I did",
+    "comps": 0.75,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "Please forgive me",
+    "comps": 0.5,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm feeling sad today",
+    "comps": 0.25,
+    "extrs": -0.75
+  },
+  {
+    "phrase": "Can we talk about this?",
+    "comps": 0,
+    "extrs": 1
+  },
+  {
+    "phrase": "Let's work together to find a solution",
+    "comps": 0.5,
+    "extrs": 1
+  },
+  {
+    "phrase": "I understand where you're coming from",
+    "comps": 0.75,
+    "extrs": 0
+  },
+  {
+    "phrase": "I value your opinion",
+    "comps": 0.5,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "Let's take a break and come back to this later",
+    "comps": 0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I don't want to fight",
+    "comps": 0.5,
+    "extrs": -1
+  },
+  {
+    "phrase": "I'm willing to compromise",
+    "comps": 0.25,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I don't agree with you, but I respect your opinion",
+    "comps": 0.5,
+    "extrs": 0
+  },
+  {
+    "phrase": "Let's take a moment to breathe",
+    "comps": 0.75,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I want to hear what you have to say",
+    "comps": 0.5,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "You did a great job",
+    "comps": 0.75,
+    "extrs": 1
+  },
+  {
+    "phrase": "I'm proud of you",
+    "comps": 1,
+    "extrs": 0.75
+  },
+  {
+    "phrase": "Don't worry, everything will be okay",
+    "comps": 1,
+    "extrs": 0
+  },
+  {
+    "phrase": "I believe in you",
+    "comps": 0.75,
+    "extrs": 0.75
+  },
+  {
+    "phrase": "I'll always be here for you",
+    "comps": 1,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I trust you",
+    "comps": 0.75,
+    "extrs": 0.25
+  },
+  {
+    "phrase": "I'm here to listen",
+    "comps": 0.75,
+    "extrs": 1
+  },
+  {
+    "phrase": "You're not alone",
+    "comps": 0.75,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I'm sorry you're going through this",
+    "comps": 1,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'm on your side",
+    "comps": 0.75,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "Let's find a way to make this work",
+    "comps": 0.5,
+    "extrs": 0.75
+  },
+  {
+    "phrase": "I want to make things right",
+    "comps": 0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'll do better next time",
+    "comps": 0.5,
+    "extrs": 0
+  },
+  {
+    "phrase": "I'm grateful for your support",
+    "comps": 1,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "You're doing great",
+    "comps": 0.75,
+    "extrs": 1
+  },
+  {
+    "phrase": "I appreciate your hard work",
+    "comps": 0.5,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I'm sorry for any inconvenience",
+    "comps": 0.75,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I respect your decision",
+    "comps": 0.5,
+    "extrs": 0
+  },
+  {
+    "phrase": "I'm here to help",
+    "comps": 0.75,
+    "extrs": 1
+  },
+  {
+    "phrase": "I'm open to your suggestions",
+    "comps": 0.5,
+    "extrs": 0.5
+  },
+  {
+    "phrase": "I'll keep that in mind",
+    "comps": 0.5,
+    "extrs": 0
+  },
+  {
+    "phrase": "I understand this is difficult for you",
+    "comps": 0.75,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'm sorry for my mistake",
+    "comps": 0.75,
+    "extrs": 0
+  },
+  {
+    "phrase": "It's not personal, I just need space",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'd rather not get involved",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'm not interested in that topic",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm not in the mood for chit-chat",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I don't want to argue about this",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I don't feel like socializing today",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I don't want to engage in conflict",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I don't feel like being around people",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm not up for a conversation right now",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm not in the mood for company",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm not interested in your drama",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I don't want to talk about that",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'm not feeling very outgoing today",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I'm not interested in this discussion",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I don't want to participate in that",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I'd rather not be involved in this",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I don't want to be part of the negativity",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+  {
+    "phrase": "I don't feel like making small talk",
+    "comps": -0.25,
+    "extrs": -0.5
+  },
+  {
+    "phrase": "I don't want to contribute to the problem",
+    "comps": -0.5,
+    "extrs": -0.25
+  },
+    {
+      "phrase": "Interrupting someone while they are speaking",
+      "extrs": 0.75,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Talking only about oneself",
+      "extrs": 0.5,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Criticizing someone",
+      "extrs": 0.25,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Arguing with someone",
+      "extrs": 0.25,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Making a sarcastic comment",
+      "extrs": 0.5,
+      "comps": -0.25
+    },
+    {
+      "phrase": "Ignoring someone's feelings",
+      "extrs": 0.25,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Talking over someone",
+      "extrs": 0.75,
+      "comps": -0.25
+    },
+    {
+      "phrase": "Bullying someone",
+      "extrs": 0.75,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Blaming someone for a mistake",
+      "extrs": 0.5,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Using profanity towards someone",
+      "extrs": 0.5,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Insulting someone's intelligence",
+      "extrs": 0.25,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Dismissing someone's opinion",
+      "extrs": 0.25,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Disrespecting someone's culture",
+      "extrs": 0.5,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Refusing to compromise",
+      "extrs": 0.5,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Talking down to someone",
+      "extrs": 0.75,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Shouting at someone",
+      "extrs": 0.75,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Insulting someone's appearance",
+      "extrs": 0.5,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Disparaging someone's character",
+      "extrs": 0.5,
+      "comps": -0.75
+    },
+    {
+      "phrase": "Belittling someone's accomplishments",
+      "extrs": 0.5,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Being dismissive of someone's emotions",
+      "extrs": 0.25,
+      "comps": -0.5
+    },
+    {
+      "phrase": "Discover the community",
+      "extrs": 0,
+      "comps": 0
+    },
+      {
+        "phrase": "I'm sure you'll figure it out eventually",
+        "extrs": -0.75,
+        "comps": -0.25
+      },
+      {
+        "phrase": "You're welcome for my help, I guess",
+        "extrs": -0.5,
+        "comps": -0.5
+      },
+      {
+        "phrase": "Just forget it, it's not worth it",
+        "extrs": -0.5,
+        "comps": -0.5
+      },
+      {
+        "phrase": "Good luck with that",
+        "extrs": -0.25,
+        "comps": -0.25
+      },
+      {
+        "phrase": "Do whatever you want, it's your life",
+        "extrs": -0.25,
+        "comps": -0.25
+      },
+      {
+        "phrase": "I don't have time for this",
+        "extrs": 0,
+        "comps": -0.75
+      },
+      {
+        "phrase": "That's not my problem, sorry",
+        "extrs": 0,
+        "comps": -0.5
+      },
+      {
+        "phrase": "I don't care what you do",
+        "extrs": 0,
+        "comps": -0.5
+      },
+      {
+        "phrase": "I'm not getting involved",
+        "extrs": 0,
+        "comps": -0.25
+      },
+      {
+        "phrase": "It's fine, I'm used to being ignored",
+        "extrs": 0,
+        "comps": -0.25
+      },
+      {
+        "phrase": "Don't worry, I'm sure you'll do great",
+        "extrs": -0.5,
+        "comps": 0
+      },
+      {
+        "phrase": "You're doing it wrong, let me show you",
+        "extrs": -0.75,
+        "comps": 0
+      },
+      {
+        "phrase": "Well, aren't you just a genius",
+        "extrs": -0.5,
+        "comps": 0
+      },
+      {
+        "phrase": "I guess I'll have to do it myself",
+        "extrs": 0,
+        "comps": -0.75
+      },
+      {
+        "phrase": "I'm not sure why you bother trying",
+        "extrs": -0.5,
+        "comps": 0
+      },
+      {
+        "phrase": "I'm not mad, I'm just disappointed",
+        "extrs": -0.5,
+        "comps": 0
+      },
+      {
+        "phrase": "You're not really helping",
+        "extrs": -0.25,
+        "comps": -0.25
+      },
+      {
+        "phrase": "Why don't you just give up already",
+        "extrs": -0.75,
+        "comps": -0.5
+      },
+      {
+        "phrase": "I'm not sure what you're trying to say",
+        "extrs": -0.25,
+        "comps": -0.25
+      },
+      {
+        "phrase": "You never listen to me",
+        "extrs": -0.25,
+        "comps": -0.25
+      },
+      {
+        "phrase": "I love meeting new people and learning about their lives.",
+        "comps": 1,
+        "extrs": 1
+      },
+      {
+        "phrase": "I always try to help others, even if it means sacrificing my own time and energy.",
+        "comps": 1,
+        "extrs": 1
+      },
+      {
+        "phrase": "I thrive in social situations and enjoy making others feel comfortable and welcome.",
+        "comps": 1,
+        "extrs": 1
+      },
+      {
+        "phrase": "I'm a good listener and I always try to understand others.",
+        "comps": 1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I prefer to help others behind the scenes rather than in the spotlight.",
+        "comps": 1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I'm not the most outgoing person, but I'm always there for my friends when they need me.",
+        "comps": 1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I prefer to be alone most of the time.",
+        "comps": -1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I don't like being around people. They just annoy me.",
+        "comps": -1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I don't really care about others. They can do whatever they want.",
+        "comps": -1,
+        "extrs": -1
+      },
+      {
+        "phrase": "I don't care about others' feelings.",
+        "comps": -1,
+        "extrs": 1
+      },
+      
+      {
+        "phrase": "I'm blunt and honest,  others.",
+        "comps": -1,
+        "extrs": 1
+      },
+      {
+        "phrase": "I prefer to focus on myself and my goals.",
+        "comps": -1,
+        "extrs": 1
+      },
+        {
+          "phrase": "I don't like to get involved in other people's problems.",
+          "comps": -0.75,
+          "extrs": -1
+        },
+        {
+          "phrase": "I'm not interested in making small talk with strangers.",
+          "comps": -0.75,
+          "extrs": -1
+        },
+        {
+          "phrase": "I can come across as aloof or distant, even when I'm around people.",
+          "comps": -0.75,
+          "extrs": -1
+        },
+        {
+          "phrase": "I prefer to keep to myself and avoid socializing.",
+          "comps": -0.5,
+          "extrs": -1
+        },
+        {
+          "phrase": "I'm not comfortable in large groups or crowded places.",
+          "comps": -0.5,
+          "extrs": -1
+        },
+        {
+          "phrase": "I don't see the point in going out of my way to help others.",
+          "comps": -0.5,
+          "extrs": -1
+        },
+        {
+          "phrase": "I'm not very good at expressing my emotions or connecting with others.",
+          "comps": -0.25,
+          "extrs": -1
+        },
+        {
+          "phrase": "I tend to avoid conflict and confrontation, even if it means not standing up for myself or others.",
+          "comps": -0.25,
+          "extrs": -1
+        },
+        {
+          "phrase": "I don't like being the center of attention or drawing attention to myself.",
+          "comps": -0.25,
+          "extrs": -1
+        },
+        {
+          "phrase": "I don't like to get involved in other people's problems.",
+          "comps": -0.75,
+          "extrs": -0.75
+        },
+        {
+          "phrase": "I prefer to keep to myself and avoid socializing.",
+          "comps": -0.75,
+          "extrs": -0.75
+        },
+        {
+          "phrase": "I often feel drained or exhausted after being around people.",
+          "comps": -0.75,
+          "extrs": -0.75
+        },
+        {
+          "phrase": "I find social situations overwhelming and anxiety-provoking.",
+          "comps": -0.75,
+          "extrs": -0.5
+        },
+        {
+          "phrase": "I'm not very talkative or outgoing, even with people I know well.",
+          "comps": -0.75,
+          "extrs": -0.5
+        },
+        {
+          "phrase": "I don't enjoy being the center of attention or receiving compliments.",
+          "comps": -0.75,
+          "extrs": -0.5
+        },
+        {
+          "phrase": "I can be reserved or shy around new people, but open up over time.",
+          "comps": -0.75,
+          "extrs": -0.25
+        },
+        {
+          "phrase": "I'm not very good at small talk or making casual conversation.",
+          "comps": -0.75,
+          "extrs": -0.25
+        },
+        {
+          "phrase": "I'm more of a listener than a talker in most social situations.",
+          "comps": -0.75,
+          "extrs": -0.25
+        },
+        {
+          "phrase": "I generally keep to myself, but I'm willing to lend a hand if someone asks for help.",
+  "comps": 0.25,
+  "extrs": 0
+        },
+        {
+          "phrase": "I enjoy meeting new people, but sometimes it's hard to connect with others.",
+  "comps": 0,
+  "extrs": 0.25
+        },
+        {
+          "phrase": "I'm not touchy-feely, but I enjoy being around people and having fun.",
+  "comps": -0.25,
+  "extrs": 0.25
+        },
+        {
+          "phrase": "I prefer spending time alone or with a small group of people, but I try to be kind and helpful to those around me.",
+  "comps": -0.25,
+  "extrs": 0
+        },
+        {
+          "phrase": "I can support others, and still be blunt and direct.",
+  "comps": -0.25,
+  "extrs": 0
+        },
+        {
+          "phrase": "I'm polite and respectful, but I'm not a fan of meeting new people.",
+  "comps": 0,
+  "extrs": -0.25
+        }
+  ]
